5b814598e76ec76f429bde5ddd580c3e7d8b790c
LauraMenendez <UO244646@uniovi.es>
2017-04-12 14:56:02 +0200

    Pruebas Travis y Codecov.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+[![Build Status](https://travis-ci.org/Arquisoft/parti1a.svg?branch=master)](https://travis-ci.org/Arquisoft/parti1a)
+[![codecov](https://codecov.io/gh/Arquisoft/parti1a/branch/master/graph/badge.svg)](https://codecov.io/gh/Arquisoft/parti1a)
+
 # parti1a
 
 
--- src/asw/Application.java
+++ src/asw/Application.java
@@ -6,10 +6,8 @@ import java.io.IOException;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class Application {
 	public static final Logger logger = Logger.getLogger(Application.class);
 
