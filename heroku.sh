b8cd396a652505dcce29aa0669ef1192fb49a01f
IgnacioEscribano <ignacioeb90@gmail.com>
2017-05-07 20:03:15 +0200

    Update heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -5,4 +5,4 @@ mkdir -p kafka && tar xzf kafka.tgz -C kafka --strip-components 1
 fi
 nohup bash -c "cd kafka && bin/zookeeper-server-start.sh config/zookeeper.properties &"
 nohup bash -c "cd kafka && bin/kafka-server-start.sh config/server.properties &"
-java $JAVA_OPTS -Dserver.port=$PORT -jar target/*.jar 
+java $JAVA_OPTS -Dserver.port=$PORT -jar target/*.jar &
