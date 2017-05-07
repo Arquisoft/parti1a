131a7e81501e0bbf1c27eda30c20367d46da194c
IgnacioEscribano <ignacioeb90@gmail.com>
2017-05-07 20:29:24 +0200

    Update heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -2,8 +2,7 @@
 if [ ! -d kafka ]; then
 wget http://apache.rediris.es/kafka/0.10.2.0/kafka_2.11-0.10.2.0.tgz -O kafka.tgz
 mkdir -p kafka && tar xzf kafka.tgz -C kafka --strip-components 1
-cd kafka/bin
-rm -r windows
+rm kafka.tgz
 fi
 nohup bash -c "cd kafka && bin/zookeeper-server-start.sh config/zookeeper.properties &"
 nohup bash -c "cd kafka && bin/kafka-server-start.sh config/server.properties &"
